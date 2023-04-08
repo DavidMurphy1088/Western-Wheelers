6dba4db4e1bea0063f695f9fd65c39f33a4c76df
David Murphy <davidp.murphy@sbcglobal.net>
2023-04-08 12:08:46 +1200

    push test 3

--- Western-Wheelers/Views/ActivityIndicator.swift
+++ Western-Wheelers/Views/ActivityIndicator.swift
@@ -1,7 +1,9 @@
 import SwiftUI
 //https://jetrockets.pro/blog/activity-indicator-in-swiftui
 struct ActivityIndicator: View {
+    
     @State private var isAnimating: Bool = false
+    
     var body: some View {
         GeometryReader { (geometry: GeometryProxy) in
             ForEach(0..<5) { index in
@@ -24,4 +26,5 @@ struct ActivityIndicator: View {
         self.isAnimating = true
         }
     }
+    
 }
